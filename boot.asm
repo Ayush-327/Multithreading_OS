3eeee9edd9993b47e9d0f9b5a4c6f162b8d0f84d
Ayush-327 <ayush327@gmail.com>
2024-08-12 16:51:55 +0530

    Protected mode

---
+++ boot.asm
@@ -0,0 +1,77 @@
+ORG 0x7c00
+BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+_start:
+    jmp short start
+    nop
+
+times 33 db 0
+
+start:
+    jmp 0:step2
+
+step2:
+    cli ; Clear interrupts
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti ; Enables interrupts
+
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
+
+;GDT
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
+
+;offset 0x8
+gdt_code:
+    dx 0xffff ; segment limit first 0-15 bits
+    dw 0      ; base first 0-15 bits
+    db 0      ; base 16-23 bits
+    db 0x9a   ; access byte
+    db 11001111b ; high 4 bit flags and the low 4 bit flags
+    db 0      ; base 24-31 bits
+
+;offset 0x10
+gdt_data:
+    dx 0xffff ; segment limit first 0-15 bits
+    dw 0      ; base first 0-15 bits
+    db 0      ; base 16-23 bits
+    db 0x92   ; access byte
+    db 11001111b ; high 4 bit flags and the low 4 bit flags
+    db 0      ; base 24-31 bits
+
+gdt_end:
+
+get_descriptor:
+    dw gdt_end - gdt_start -1
+    dd gdt_start
+
+[BITS 32]
+load 32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
+    jmp $
+
+times 510 - ($ - $$) db 0
+dw 0xAA55
