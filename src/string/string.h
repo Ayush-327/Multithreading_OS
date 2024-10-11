bcd6ebd8b510bc0c279004b72c2326930307afb7
Ayush-327 <ayush327@gmail.com>
2024-10-12 00:58:51 +0530

    Implemented VFS fopen

--- src/string/string.h
+++ src/string/string.h
@@ -8,5 +8,10 @@ bool isdigit(char c);
 char* strcpy(char* dest, const char* src);
 int tonumericdigit(char c);
 int strnlen(const char* ptr, int max);
+int istrncmp(const char* s1, const char* s2, int n);
+int strncmp(const char* s1, const char* s2, int n);
+int strnlen_terminator(const char* str, int max, char terminator);
+char tolower(char s1);
+
 
 #endif
