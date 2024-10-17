c9147a28666a966428be40447f60a830609a949a
Ayush-327 <ayush327@gmail.com>
2024-10-18 01:42:59 +0530

    Implemented VFS Fseek

--- src/fs/file.c
+++ src/fs/file.c
@@ -182,6 +182,20 @@ out:
     return res;
 }
 
+int fseek(int fd, int offset, FILE_SEEK_MODE whence)
+{
+    int res = 0;
+    struct file_descriptor* desc = file_get_descriptor(fd);
+    if (!fd)
+    {
+        res= -EIO;
+        goto out;
+    }
+    res = desc->filesystem->seek(desc->private, offset, whence);
+out:
+    return res;
+}
+
 int fread(void* ptr, uint32_t size, uint32_t nmemb, int fd)
 {
     int res = 0;
